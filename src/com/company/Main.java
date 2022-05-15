d9e20f64223301b6c8347a945f3f4264f6279b9f
Ruslan <mutygullin@yandexlyceum.ru>
2022-05-15 21:00:21 +0300

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // VCS - Version Control System.
+        // git
+        // commit, branch,  push, pull, (merge, checkout, fork)
+        // github, gitlab
+        // git flow
+        System.out.println("Hello world!");
+
+    }
+}
